38f51deef5092bc7925f0bc27e2de2fd62b6f09e
dladuke <dladukedev@gmail.com>
2023-10-20 21:28:09 -0400

    Initial Commit

---
+++ BibleYearTracker/BibleYearTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleYearTrackerApp.swift
+//  BibleYearTracker
+//
+//  Created by Donovan LaDuke on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BibleYearTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
